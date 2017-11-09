e0cbcbfc0524a1933380b40c1b00ce6ca16e2415
Maximilian Stoiber <contact@mxstbr.com>
2017-11-09 11:51:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# mxstbr/dotfiles
+Just a bunch of dotfiles.
+
+## Install
+Clone and symlink or install with [ellipsis][ellipsis]:
+
+```
+$ ellipsis install mxstbr/dotfiles
+```
+
+[ellipsis]: http://ellipsis.sh
---
+++ ellipsis.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# mxstbr/dotfiles ellipsis package
+
+# The following hooks can be defined to customize behavior of your package:
+# pkg.install() {
+#     fs.link_files $PKG_PATH
+# }
+
+# pkg.push() {
+#     git.push
+# }
+
+# pkg.pull() {
+#     git.pull
+# }
+
+# pkg.installed() {
+#     git.status
+# }
+#
+# pkg.status() {
+#     git.diffstat
+# }
